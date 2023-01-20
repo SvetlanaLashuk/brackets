e8f2eac940dfeadc368e3eee190d33a98caa9487
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-01-20 09:20:07 +0300

    feat: add task implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let elem;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    elem = bracketsConfig[i].join('');
+    if (str.includes(elem)) {
+      str = str.replaceAll(elem, '');
+      if (str.length === 0) {
+        return true;
+      } else {
+        i = -1;
+      }
+    }
+  }
+  return false;
 }
